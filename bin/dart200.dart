c36774cf6c8a632464d6746c65a03aa24f4bc57b
gituser <relay67@gmail.com>
2023-07-04 07:11:24 +0900

    sample updates

--- bin/dart200.dart
+++ bin/dart200.dart
@@ -1,5 +1,5 @@
 import 'package:dart200/dart200.dart' as dart200;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart200.calculate()}!');
+  dart200.ex001_010();
 }
--- lib/dart200.dart
+++ lib/dart200.dart
@@ -1,3 +1,10 @@
-int calculate() {
-  return 6 * 7;
+
+/* ref link https://wikidocs.net/7014 */
+
+void ex001_010() {
+  print('Hello World!');        // ex001
+  print('Mary\'s cosmetics' );
+  print('신씨가 소리질렀다. "도독이야".');
+
 }
+
