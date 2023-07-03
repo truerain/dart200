29c3fa218204190bb2fd66697fbc1e71640715be
gituser <relay67@gmail.com>
2023-07-04 06:42:40 +0900

    create dart

---
+++ bin/dart200.dart
@@ -0,0 +1,5 @@
+import 'package:dart200/dart200.dart' as dart200;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart200.calculate()}!');
+}
---
+++ test/dart200_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart200/dart200.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
